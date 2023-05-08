b893144a458ff5006c8a9ee5db5e807bda8d8cff
Md. Golam Kausar <golam@mirinfosys.com>
2023-05-08 17:23:53 +0600

    start

---
+++ app/src/main/java/com/kausar/roomwordssample/MainActivity.java
@@ -0,0 +1,39 @@
+package com.kausar.roomwordssample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.kausar.roomwordssample.room.Word;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WordViewModel mWordViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWordViewModel = new ViewModelProvider(this).get(WordViewModel.class);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final WordListAdapter adapter = new WordListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mWordViewModel.getAllWords().observe(this, new Observer<List<Word>>() {
+            @Override
+            public void onChanged(List<Word> words) {
+                adapter.setWords(words);
+            }
+        });
+
+    }
+}
