3d5eb7ce4be05cc7983a3cf988950d5ba1da1848
Md. Golam Kausar <golam@mirinfosys.com>
2023-05-09 10:08:53 +0600

    insert a new word

--- app/src/main/java/com/kausar/roomwordssample/MainActivity.java
+++ app/src/main/java/com/kausar/roomwordssample/MainActivity.java
@@ -1,18 +1,27 @@
 package com.kausar.roomwordssample;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.kausar.roomwordssample.room.Word;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
 
     private WordViewModel mWordViewModel;
 
@@ -35,5 +44,50 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.fav).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewWordActivity.class);
+//                startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+                launchActivity.launch(intent);
+            }
+        });
+
     }
+
+    private ActivityResultLauncher<Intent> launchActivity = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+
+                    if (result.getResultCode() == RESULT_OK) {
+                        Word word = new Word(result.getData().getStringExtra(NewWordActivity.EXTRA_REPLY));
+                        mWordViewModel.insert(word);
+                    } else {
+                        Toast.makeText(
+                                getApplicationContext(),
+                                R.string.empty_not_saved,
+                                Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            }
+    );
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+//            Word word = new Word(data.getStringExtra(NewWordActivity.EXTRA_REPLY));
+//            mWordViewModel.insert(word);
+//        } else {
+//            Toast.makeText(
+//                    getApplicationContext(),
+//                    R.string.empty_not_saved,
+//                    Toast.LENGTH_LONG).show();
+//        }
+//
+//    }
 }
--- app/src/main/java/com/kausar/roomwordssample/room/WordDao.java
+++ app/src/main/java/com/kausar/roomwordssample/room/WordDao.java
@@ -4,6 +4,7 @@ package com.kausar.roomwordssample.room;
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.List;
 @Dao
 public interface WordDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insert(Word word);
 
     @Query("DELETE FROM word_table")
